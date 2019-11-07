ccf9ad3984da28e9e10d96aef84a702006cecc00
gfraga <gabriel.fraga@cielo.com.br>
2019-11-07 10:51:09 -0300

    added gyro

---
+++ Assets/Skating/ChangeSchene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSchene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("gyro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Skating/GyroControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+
+    private bool isGyroEnable;
+    private Gyroscope gyro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGyroEnable = EnableGyro();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation  = gyro.attitude;
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+}
